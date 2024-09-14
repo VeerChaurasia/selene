203f8cb329148c3476e2a8ad0e71c3a53516ef00
VeerChaurasia <veerchaurasia2005@gmail.com>
2024-09-15 01:35:54 +0530

    Implemented Beacon.go and basic.go

--- consensus/types/basic.go
+++ consensus/types/basic.go
@@ -1,10 +1,129 @@
 package types
 
+import (
+	"encoding/hex"
+	"fmt"
+)
 
+// ByteList represents a list of bytes with a maximum capacity.
+type ByteList struct {
+	data    []byte
+	maxSize int
+}
 
+// NewByteList creates a new ByteList with the given maximum size.
+func NewByteList(maxSize int) *ByteList {
+	return &ByteList{
+		data:    make([]byte, 0, maxSize),
+		maxSize: maxSize,
+	}
+}
 
-type ByteList struct{
+// FromBytes creates a ByteList from a byte slice.
+func FromBytes(data []byte, maxSize int) (*ByteList, error) {
+	if len(data) > maxSize {
+		return nil, fmt.Errorf("data exceeds maximum size of %d", maxSize)
+	}
+	return &ByteList{
+		data:    append([]byte(nil), data...),
+		maxSize: maxSize,
+	}, nil
+}
 
+// Append adds bytes to the ByteList.
+func (bl *ByteList) Append(data ...byte) error {
+	if len(bl.data)+len(data) > bl.maxSize {
+		return fmt.Errorf("appending would exceed maximum size of %d", bl.maxSize)
+	}
+	bl.data = append(bl.data, data...)
+	return nil
+}
+
+// Bytes returns the underlying byte slice.
+func (bl *ByteList) Bytes() []byte {
+	return bl.data
+}
+
+// Len returns the current length of the ByteList.
+func (bl *ByteList) Len() int {
+	return len(bl.data)
+}
+
+// MaxSize returns the maximum size of the ByteList.
+func (bl *ByteList) MaxSize() int {
+	return bl.maxSize
+}
+
+// String returns a hexadecimal representation of the ByteList.
+func (bl *ByteList) String() string {
+	return "0x" + hex.EncodeToString(bl.data)
+}
+
+// MarshalText implements the encoding.TextMarshaler interface.
+func (bl *ByteList) MarshalText() ([]byte, error) {
+	return []byte(bl.String()), nil
+}
+
+// UnmarshalText implements the encoding.TextUnmarshaler interface.
+func (bl *ByteList) UnmarshalText(text []byte) error {
+	if len(text) >= 2 && text[0] == '0' && (text[1] == 'x' || text[1] == 'X') {
+		text = text[2:]
+	}
+	data, err := hex.DecodeString(string(text))
+	if err != nil {
+		return err
+	}
+	if len(data) > bl.maxSize {
+		return fmt.Errorf("data exceeds maximum size of %d", bl.maxSize)
+	}
+	bl.data = data
+	return nil
+}
+
+// ByteVector represents a fixed-size vector of bytes.
+type ByteVector[N int] struct {
+	data [N]byte
+}
+
+// NewByteVector creates a new ByteVector from a slice of bytes.
+func NewByteVector[N int](data []byte) (ByteVector[N], error) {
+	if len(data) != N {
+		return ByteVector[N]{}, fmt.Errorf("invalid data length: expected %d, got %d", N, len(data))
+	}
+	var bv ByteVector[N]
+	copy(bv.data[:], data)
+	return bv, nil
+}
+
+// Bytes returns the underlying byte array as a slice.
+func (bv ByteVector[N]) Bytes() []byte {
+	return bv.data[:]
+}
+
+// String returns a hexadecimal representation of the ByteVector.
+func (bv ByteVector[N]) String() string {
+	return "0x" + hex.EncodeToString(bv.data[:])
+}
+
+// MarshalText implements the encoding.TextMarshaler interface.
+func (bv ByteVector[N]) MarshalText() ([]byte, error) {
+	return []byte(bv.String()), nil
+}
+
+// UnmarshalText implements the encoding.TextUnmarshaler interface.
+func (bv *ByteVector[N]) UnmarshalText(text []byte) error {
+	if len(text) >= 2 && text[0] == '0' && (text[1] == 'x' || text[1] == 'X') {
+		text = text[2:]
+	}
+	data, err := hex.DecodeString(string(text))
+	if err != nil {
+		return err
+	}
+	if len(data) != N {
+		return fmt.Errorf("invalid data length: expected %d, got %d", N, len(data))
+	}
+	copy(bv.data[:], data)
+	return nil
 }
 
 type Address = [20] byte;
--- consensus/types/beacon.go
+++ consensus/types/beacon.go
@@ -1,39 +1,203 @@
 package types
-
-
-type BeaconBlock struct{}
-type BeaconBlockBody struct{}// 1 method
-func (b BeaconBlockBody) def(){}//default
-
-type SignedBlsToExecutionChange struct{}
-type BlsToExecutionChange struct{}
-
-type ExecutionPayload struct{}// 1 method
-func (exe ExecutionPayload) def(){}// default
-
-type Withdrawwal struct{}
-type ProposalSlashing struct{}
-type SignedBeaconBlockHeader struct{}
-type AttesterSlashing struct{}
-type IndexedAttestation struct{}
-type Attestation struct {}
-type AttestationData struct{}
-type Checkpoint struct{}
-type SignedVoluntaryExit struct{}
-type VoluntaryExit struct{}
-type Deposit struct{}
-type DepositData struct{}
-type Eth1Data struct{}
-type Updatec struct{}
-type FinalityUpdate struct{}
-type OptimisticUpdate struct{}
-type Bootstrap struct{}
-
-type Header struct{}
-type SyncCommittee struct{}
-type SyncAggregate struct{}
-
-type GenericUpdate struct{}
-func (g GenericUpdate)from(){
-
+type BeaconBlock struct {
+	Slot          uint64
+	ProposerIndex uint64
+	ParentRoot    Bytes32
+	StateRoot     Bytes32
+	Body          BeaconBlockBody
+}
+type BeaconBlockBody struct {
+	RandaoReveal          SignatureBytes
+	Eth1Data              Eth1Data
+	Graffiti              Bytes32
+	ProposerSlashings     [16]ProposerSlashing
+	AttesterSlashings     [2]AttesterSlashing
+	Attestations          [128]Attestation
+	Deposits              [16]Deposit
+	VoluntaryExits        [16]SignedVoluntaryExit
+	SyncAggregate         SyncAggregate
+	ExecutionPayload      ExecutionPayload
+	BlsToExecutionChanges [16]SignedBlsToExecutionChange // For Capella and Deneb
+	BlobKzgCommitments    [4096]ByteVector               // For Deneb
+}
+func (b BeaconBlockBody) Default() BeaconBlockBody {
+	return BeaconBlockBody{}
+}
+type SignedBlsToExecutionChange struct {
+	Message   BlsToExecutionChange
+	Signature SignatureBytes
+}
+type BlsToExecutionChange struct {
+	ValidatorIndex     uint64
+	FromBlsPubkey      BLSPubKey
+	ToExecutionAddress Address
+}
+type ExecutionPayload struct {
+	ParentHash    Bytes32
+	FeeRecipient  Address
+	StateRoot     Bytes32
+	ReceiptsRoot  Bytes32
+	LogsBloom     LogsBloom
+	PrevRandao    Bytes32
+	BlockNumber   uint64
+	GasLimit      uint64
+	GasUsed       uint64
+	Timestamp     uint64
+	ExtraData     ByteList
+	BaseFeePerGas uint64 // Assuming U256 is a uint64
+	BlockHash     Bytes32
+	Transactions  []ByteList   // List of transactions with a max length
+	Withdrawals   []Withdrawal // For Capella and Deneb
+	BlobGasUsed   uint64       // For Deneb
+	ExcessBlobGas uint64       // For Deneb
+}
+func (e ExecutionPayload) Default() ExecutionPayload {
+	// Implement default initialization if needed
+	return ExecutionPayload{}
+}
+type Withdrawal struct {
+	Index          uint64
+	ValidatorIndex uint64
+	Address        Address
+	Amount         uint64
+}
+type ProposerSlashing struct {
+	SignedHeader1 SignedBeaconBlockHeader
+	SignedHeader2 SignedBeaconBlockHeader
+}
+type SignedBeaconBlockHeader struct {
+	Message   BeaconBlockHeader
+	Signature SignatureBytes
+}
+type BeaconBlockHeader struct {
+	Slot          uint64
+	ProposerIndex uint64
+	ParentRoot    Bytes32
+	StateRoot     Bytes32
+	BodyRoot      Bytes32
+}
+type AttesterSlashing struct {
+	Attestation1 IndexedAttestation
+	Attestation2 IndexedAttestation
+}
+type IndexedAttestation struct {
+	AttestingIndices [2048]uint64
+	Data             AttestationData
+	Signature        SignatureBytes
+}
+type Attestation struct {
+	AggregationBits [2048]byte
+	Data            AttestationData
+	Signature       SignatureBytes
+}
+type AttestationData struct {
+	Slot            uint64
+	Index           uint64
+	BeaconBlockRoot Bytes32
+	Source          Checkpoint
+	Target          Checkpoint
+}
+type Checkpoint struct {
+	Epoch uint64
+	Root  Bytes32
+}
+type SignedVoluntaryExit struct {
+	Message   VoluntaryExit
+	Signature SignatureBytes
+}
+type VoluntaryExit struct {
+	Epoch          uint64
+	ValidatorIndex uint64
+}
+type Deposit struct {
+	Proof [33]Bytes32
+	Data  DepositData
+}
+type DepositData struct {
+	Pubkey                BLSPubKey
+	WithdrawalCredentials Bytes32
+	Amount                uint64
+	Signature             SignatureBytes
+}
+type Eth1Data struct {
+	DepositRoot  Bytes32
+	DepositCount uint64
+	BlockHash    Bytes32
+}
+type Bootstrap struct {
+	Header                     Header
+	CurrentSyncCommittee       SyncCommittee
+	CurrentSyncCommitteeBranch []Bytes32
+}
+type Update struct {
+	AttestedHeader          Header
+	NextSyncCommittee       SyncCommittee
+	NextSyncCommitteeBranch []Bytes32
+	FinalizedHeader         Header
+	FinalityBranch          []Bytes32
+	SyncAggregate           SyncAggregate
+	SignatureSlot           uint64
+}
+type FinalityUpdate struct {
+	AttestedHeader  Header
+	FinalizedHeader Header
+	FinalityBranch  []Bytes32
+	SyncAggregate   SyncAggregate
+	SignatureSlot   uint64
+}
+type OptimisticUpdate struct {
+	AttestedHeader Header
+	SyncAggregate  SyncAggregate
+	SignatureSlot  uint64
+}
+type Header struct {
+	Slot          uint64
+	ProposerIndex uint64
+	ParentRoot    Bytes32
+	StateRoot     Bytes32
+	BodyRoot      Bytes32
+}
+type SyncCommittee struct {
+	Pubkeys         [512]BLSPubKey
+	AggregatePubkey BLSPubKey
+}
+type SyncAggregate struct {
+	SyncCommitteeBits      []byte 
+	SyncCommitteeSignature SignatureBytes
+}
+type GenericUpdate struct {
+	AttestedHeader          Header
+	SyncAggregate           SyncAggregate
+	SignatureSlot           uint64
+	NextSyncCommittee       *SyncCommittee
+	NextSyncCommitteeBranch *[]Bytes32
+	FinalizedHeader         *Header
+	FinalityBranch          *[]Bytes32
+}
+func (g *GenericUpdate) FromUpdate(update *Update) {
+	g.AttestedHeader = update.AttestedHeader
+	g.SyncAggregate = update.SyncAggregate
+	g.SignatureSlot = update.SignatureSlot
+	g.NextSyncCommittee = &update.NextSyncCommittee
+	g.NextSyncCommitteeBranch = &update.NextSyncCommitteeBranch
+	g.FinalizedHeader = &update.FinalizedHeader
+	g.FinalityBranch = &update.FinalityBranch
+}
+func (g *GenericUpdate) FromFinalityUpdate(update *FinalityUpdate) {
+	g.AttestedHeader = update.AttestedHeader
+	g.SyncAggregate = update.SyncAggregate
+	g.SignatureSlot = update.SignatureSlot
+	g.NextSyncCommittee = nil
+	g.NextSyncCommitteeBranch = nil
+	g.FinalizedHeader = &update.FinalizedHeader
+	g.FinalityBranch = &update.FinalityBranch
+}
+func (g *GenericUpdate) FromOptimisticUpdate(update *OptimisticUpdate) {
+	g.AttestedHeader = update.AttestedHeader
+	g.SyncAggregate = update.SyncAggregate
+	g.SignatureSlot = update.SignatureSlot
+	g.NextSyncCommittee = nil
+	g.NextSyncCommitteeBranch = nil
+	g.FinalizedHeader = nil
+	g.FinalityBranch = nil
 }
